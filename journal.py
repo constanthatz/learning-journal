feccd36e911f815f993bb314022312a34e004a2e
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-05 15:34:56 -0800

    add view function and jinja2 file to handle login page.

--- journal.py
+++ journal.py
@@ -13,6 +13,7 @@ from pyramid.httpexceptions import HTTPFound, HTTPInternalServerError
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from cryptacular.bcrypt import BCRYPTPasswordManager
+from pyramid.security import remember, forget
 
 
 DB_SCHEMA = """
@@ -159,6 +160,24 @@ def do_login(request):
         return manager.check(hashed, password)
 
 
+@view_config(route_name='login', renderer="templates/login.jinja2")
+def login(request):
+    """authenticate a user by username/password"""
+    username = request.params.get('username', '')
+    error = ''
+    if request.method == 'POST':
+        error = "Login Failed"
+        authenticated = False
+        try:
+            authenticated = do_login(request)
+        except ValueError as e:
+            error = str(e)
+
+        if authenticated:
+            headers = remember(request, username)
+            return HTTPFound(request.route_url('home'), headers=headers)
+
+    return {'error': error, 'username': username}
 
 if __name__ == '__main__':
     app = main()
