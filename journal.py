d47192a72503206fa19d6173f4ffdfe523582e8d
henrykh <henryhowes@gmail.com>
2015-02-09 17:29:55 -0800

    moved detail view

--- journal.py
+++ journal.py
@@ -136,6 +136,14 @@ def read_entries(request):
     entries = [dict(zip(keys, row)) for row in cursor.fetchall()]
     return {'entries': entries}
 
+@view_config(route_name='detail', renderer='templates/detail.jinja2')
+def read_entry(request):
+    """return a list of all entries as dicts"""
+    cursor = request.db.cursor()
+    cursor.execute(DB_ENTRIES_LIST)
+    keys = ('id', 'title', 'text', 'created')
+    entries = [dict(zip(keys, row)) for row in cursor.fetchall()]
+    return {'entries': entries}
 
 @view_config(route_name='add', request_method='POST')
 def add_entry(request):
@@ -191,11 +199,12 @@ if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=port)
 
 
-@view_config(route_name='detail', renderer='templates/detail.jinja2')
-def read_entry(request):
-    """return a list of all entries as dicts"""
-    cursor = request.db.cursor()
-    cursor.execute(DB_ENTRIES_LIST)
-    keys = ('id', 'title', 'text', 'created')
-    entries = [dict(zip(keys, row)) for row in cursor.fetchall()]
-    return {'entries': entries}
+# @view_config(route_name='detail', renderer='templates/detail.jinja2')
+# def read_entry(request):
+#     """return a list of all entries as dicts"""
+#     return {'test': 'test'}
+#     # cursor = request.db.cursor()
+#     # cursor.execute(DB_ENTRIES_LIST)
+#     # keys = ('id', 'title', 'text', 'created')
+#     # entries = [dict(zip(keys, row)) for row in cursor.fetchall()]
+#     # return {'entries': entries}
