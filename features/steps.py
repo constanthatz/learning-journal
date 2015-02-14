78879d2cf013e1fbb545bae3126e98d209341bad
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-13 16:29:21 -0800

    add edit entry test.

--- features/steps.py
+++ features/steps.py
@@ -15,12 +15,12 @@ CREATE TABLE IF NOT EXISTS entries (
     created TIMESTAMP NOT NULL
 )
 """
-world.INSERT_ENTRY = """INSERT INTO entries (title, text, created) VALUES (%s, %s, %s)
+INSERT_ENTRY = """INSERT INTO entries (title, text, created) VALUES (%s, %s, %s)
 """
 
-TEST_DSN = 'dbname=test_learning_journal user=henryhowes'
-world.INPUT_BTN = '<input type="submit" value="Share" name="Share"/>'
-world.READ_ENTRY = """SELECT * FROM entries
+TEST_DSN = 'dbname=test_learning_journal user=chatzis'
+INPUT_BTN = '<input type="submit" value="Share" name="Share"/>'
+READ_ENTRY = """SELECT * FROM entries where id=%s
 """
 
 settings = {'db': TEST_DSN}
@@ -37,15 +37,15 @@ def run_query(db, query, params=(), get_results=True):
     return results
 
 
-@before.all
-def init_db():
+@before.each_scenario
+def init_db(scenario):
     with closing(connect_db(settings)) as db:
         db.cursor().execute(world.DB_SCHEMA)
         db.commit()
 
 
-@after.all
-def clear_db(total):
+@after.each_scenario
+def clear_db(scenario):
     with closing(connect_db(settings)) as db:
         db.cursor().execute("DROP TABLE entries")
         db.commit()
@@ -61,24 +61,33 @@ def app(scenario):
     world.app = TestApp(app)
 
 
-@after.each_scenario
-def clear_entries(scenario):
-    with closing(connect_db(settings)) as db:
-        db.cursor().execute("DELETE FROM entries")
-        db.commit()
+# @after.each_scenario
+# def clear_entries(scenario):
+#     with closing(connect_db(settings)) as db:
+#         db.cursor().execute("DELETE FROM entries")
+#         db.commit()
 
 
 @world.absorb
-def entry(app):
+def add_entry(app):
     """provide a single entry in the database"""
     now = datetime.datetime.utcnow()
     expected = ('Test Title', 'Test Text', now)
     with closing(connect_db(settings)) as db:
-        world.run_query(db, world.INSERT_ENTRY, expected, False)
+        world.run_query(db, INSERT_ENTRY, expected, False)
         db.commit()
 
     return expected
 
+
+@world.absorb
+def login_helper(username, password, app):
+    """encapsulate app login for reuse in tests
+
+    Accept all status codes so that we can make assertions in tests
+    """
+    login_data = {'username': username, 'password': password}
+    return app.post('/login', params=login_data, status='*')
 
 
 
@@ -127,22 +136,48 @@ def the_post(step, id):
 
 @step('when I enter the url /detail/(\d+)')
 def test_detail_listing(step, id):
-
-
-    # item = run_query(req_context.db, world.READ_ENTRY)
-    world.entry = world.entry(world.app)
+    world.entry = world.add_entry(world.app)
     world.response = world.app.get('/detail/{}'.format(id))
-   
 
-@step('Then I see the content of that post')
-def compare(step):
+
+@step('Then I see the detail page and the content of that post')
+def detial_compare(step):
     assert world.response.status_code == 200
-
 
     actual = world.response.body
     for expected in world.entry[:2]:
         assert expected in actual
 
+
+@step('that I want to edit post (\d+)')
+def the_edit(step, id):
+    world.number = int(id)
+
+
+@step('when I enter the url /editview/(\d+)')
+def test_edit_listing(step, id):
+    world.entry = world.add_entry(world.app)
+    world.entry_data = {
+        'title': 'Hello there',
+        'text': 'This is a post',
+    }
+
+    username, password = ('admin', 'secret')
+    login_helper(username, password, world.app)
+
+    world.response_post = world.app.post(
+        '/editview/{}'.format(id), params=world.entry_data, status='3*')
+    world.new_entry = world.add_entry(world.app)
+    world.response_get = world.app.get('/detail/{}'.format(id))
+
+
+@step('Then I can see the new edit page and edit the entry')
+def edit_compare(step):
+    assert world.response_get.status_code == 200
+    world.entry_data
+    actual = world.response_get.body
+    for expected in world.entry_data:
+        assert world.entry_data[expected] in actual
 
 
 # @step("that I'm on the detail view for a post")
