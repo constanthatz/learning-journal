ed5359b2447e10e606d8e8809d59f8e6f77d70e3
henrykh <henryhowes@gmail.com>
2015-02-18 15:44:30 -0800

    edit grabs id from url

--- static/main.js
+++ static/main.js
@@ -3,11 +3,7 @@ $(document).ready(function () {
       event.preventDefault();
       add_post();
     });
-  $('#editLink').on("click", function(event){
-      event.preventDefault();
-      open_edit();
-    });
-})
+});
 
 
 
@@ -32,7 +28,20 @@ function open_edit() {
       dataType: 'json',
       data: {'id': id},
       success: open_edit_success,
-      error: open_edit_fail
+    });
+}
+
+function edit_post() {
+    var title = $('#title').val();
+    var text = $('#text').val();
+    var split_path = window.location.pathname.split("/");
+    var id = split_path[split_path.length-1];
+    $.ajax({
+      url: '/editview',
+      type: 'POST',
+      dataType: 'json',
+      data: {'title': title, 'text': text, 'id':},
+      success: edit_success
     });
 }
 
@@ -48,9 +57,18 @@ function success(entry){
     $('.add_entry').after(html);
 }
 
+function edit_success(entry){
+    var template = '<article class="entry" id="entry{{id}}">'+
+                      '<h3><a href= "/detail/{{id}}"><h3>{{title}}</a></h3>'+
+                      '<p class="dateline">{{created}}'+
+                      '<div class="entry_body">{{{text}}}</div>'+
+                    '</article>';
+
+    var html = Mustache.to_html(template, entry);
+    $('#content').html(html);
+}
 
 function open_edit_success(entry){
-  console.log("success")
   var template = '<aside><form action="{{ request.route_url("editview", id=entry.id) }}" method="POST" class="edit_entry">'+
                  '<div class="field"><label for="title">Title</label>'+
                  '<input type="text" value="{{title}}" size="30" name="title" id="title"/></div>'+
@@ -60,14 +78,12 @@ function open_edit_success(entry){
 
   var html = Mustache.to_html(template, entry);
   $('#content').html(html);
+  $('.edit_entry').on('submit', function(event){
+      event.preventDefault();
+      edit_post();
+  });
 }
 
-
-
-function open_edit_fail(entry){
-  console.log("fail");
-  console.log(entry);
-}
 
 
 
